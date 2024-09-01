3aa65ae8dab3036598db15d0948c3fc685346090
Fu Lim <87457764+Fulim13@users.noreply.github.com>
2024-09-01 22:34:45 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cxc0icgn0rmm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "fulim-employee"
+customregion = "us-east-1"
